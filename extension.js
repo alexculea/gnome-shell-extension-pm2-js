621bff94a0811214a6324169b6d7db0992910b8c
Alex Culea <16594531+alexculea@users.noreply.github.com>
2020-04-01 23:51:58 +0300

    Fix open/close behavior and optimize UI creation

--- extension.js
+++ extension.js
@@ -1,14 +1,6 @@
 // TODO: Refactor with ES6 class instead of Lang
-// TODO: Investigate and possibly remove menu.open() in show()
-// TODO: Update README.md for publishing
 // TODO: Plan what to do about that JSON.parse in PM2 lib (it's sync and seems to be getting big inputs)
-// TODO: Plan Roadmap for 1.1 with
-// - don't close menu as soon as toggle is clicked, instead change toggle after command completed and then close the menu
-// - fix clicking the button while the menu is visible makes the menu recreate
-// 1.2:
-// - add show logs button for each entry, open the user default terminal instead of hardcoding
-// - convert shell command running to async (maybe even include it in 1.0?)
-// - show loading indicator if startup takes more than 0.5s
+
 const Lang = imports.lang;
 const Main = imports.ui.main;
 
@@ -20,54 +12,56 @@ const PM2 = Me.imports.lib.pm2;
 const PM2ProcessManager = new Lang.Class({
     Name: 'PM2ProcessManager',
     ui: {},
+    menuVisible: false,
 
     _init: function() {
         this.createPanelMenu();
     },
-    createPanelMenu: function() {
+    createPanelMenu: function () {
       this.ui = Ui.createStatusPanel({ iconPath: Me.path + '/assets/pm2-logo-light.svg' }, Lang.bind(this, function() {
-        this.show();
+        if (this.menuVisible) {
+          this.updateProcessList();
+        }
       }));
+
+      const { container: { menu } } = this.ui;
+      const all = Ui.createSubMenu('All');
+
+      all.menu.addMenuItem(Ui.createSimpleMenuItem("Start all", Lang.bind(this, this.comandForAll.bind(this, 'start'))));
+      all.menu.addMenuItem(Ui.createSimpleMenuItem("Stop all", Lang.bind(this, this.comandForAll.bind(this, 'stop'))));
+      all.menu.addMenuItem(Ui.createSimpleMenuItem("Resurrect", Lang.bind(this, this.comandForAll.bind(this, 'resurrect'))));
+
+      menu.connect('open-state-changed', Lang.bind(this, (menu, isOpen) => { this.menuVisible = isOpen }));
+      menu.addMenuItem(Ui.createSeparatorMenuItem());      
+      menu.addMenuItem(all);
 
       Main.panel.addToStatusArea('pm2ProcessManager', this.ui.container);
     },
-    show: async function() {
-      const { container } = this.ui;
-      const { menu } = container;
+    updateProcessList: async function () {
+      const { section } = this.ui;
       try {
-        menu.removeAll();
+        section.removeAll();
         const processes = await PM2.getProcesses() || [];
-        for (const process of processes) {
+        processes.forEach(process => {
           const { name, active } = PM2.getProcessInfo(process);
-          menu.addMenuItem(
-            Ui.createToggleMenuItem(
-              name, 
-              active, 
-              Lang.bind(this, this.toggleProcess.bind(this, process))
-            )
-          );
-        }
+          section.addMenuItem(Ui.createToggleMenuItem(
+            name, 
+            active, 
+            Lang.bind(this, this.toggleProcess.bind(this, process))
+          ));
+        });
 
         if (!processes.length) {
-          menu.addMenuItem(Ui.createSimpleMenuItem('No PM2 processes.'))  
+          const item = Ui.createSimpleMenuItem('No PM2 processes.');
+          section.addMenuItem(item)
         }
-
-        menu.addMenuItem(Ui.createSeparatorMenuItem());
-
-        const all = Ui.createSubMenu('All');
-        all.menu.addMenuItem(Ui.createSimpleMenuItem("Start all", Lang.bind(this, this.comandForAll.bind(this, 'start'))));
-        all.menu.addMenuItem(Ui.createSimpleMenuItem("Stop all", Lang.bind(this, this.comandForAll.bind(this, 'stop'))));
-        all.menu.addMenuItem(Ui.createSimpleMenuItem("Resurrect", Lang.bind(this, this.comandForAll.bind(this, 'resurrect'))));
-        menu.addMenuItem(all);
-        menu.open();
-        return true;
       } catch (e) {
-        menu.addMenuItem(Ui.createSimpleMenuItem('Error getting data from PM2. See gnome-shell logs.'))
+        section.addMenuItem(Ui.createSimpleMenuItem('Error getting data from PM2. See gnome-shell logs.'))
         Lib.log(`Error reading PM2 processes:`);
         Lib.log(e);
       }
     },
-    toggleProcess: async function(process, menuItem, toggled) {
+    toggleProcess: async function (process, menuItem, toggled) {
       try {
         if (toggled) {
           await PM2.startProcess(process);
@@ -80,7 +74,7 @@ const PM2ProcessManager = new Lang.Class({
         Lib.log(e);
       }
     },
-    comandForAll: async function(cmd) {
+    comandForAll: async function (cmd) {
       try {
         const actions = {
           start: PM2.startAll,
@@ -93,7 +87,7 @@ const PM2ProcessManager = new Lang.Class({
         Lib.log(e);
       }
     },
-    destroy: function() {
+    destroy: function () {
       this.ui.container.destroy();
     }
 });
