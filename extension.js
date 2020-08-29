4acdcde440baebca68d6b4e77a0b9d72851b5612
Alex Culea <16594531+alexculea@users.noreply.github.com>
2020-08-29 18:43:08 +0300

    Converts Lang.Class to ES6 JS Class

--- extension.js
+++ extension.js
@@ -1,123 +1,141 @@
-// TODO: Refactor with ES6 class instead of Lang
 // TODO: Plan what to do about that JSON.parse in PM2 lib (it's sync and seems to be getting big inputs)
+// TODO: Convert initCmdPromise to a be only a promise and not null
 const Lang = imports.lang;
 const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
 
+const GObject = imports.gi.GObject;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Lib = Me.imports.lib.util;
 const Ui = Me.imports.lib.ui;
 const PM2 = Me.imports.lib.pm2;
 
-const PM2ProcessManager = new Lang.Class({
-  Name: 'PM2ProcessManager',
-  ui: {},
+const STATUS_AREA_ROLE = 'pm2-manager';
 
-  // promise holding the initial process retrieval call from pm2
-  // gets set to null when no execution is in progress
-  initCmdPromise: null, 
+var PM2ProcessManager = GObject.registerClass(
+  class SystemMenu extends PanelMenu.Button {
+    _init() {
+      try {
+        super._init(0, _('PM2 Process Manager'));
 
-  processes: [],
+        this.Name = 'PM2ProcessManager';
+        this.ui = {};
+        this.testContainer = new PanelMenu.Button(0,0);
+    
+        // promise holding the initial process retrieval call from pm2
+        // gets set to null when no execution is in progress
+        this.initCmdPromise = null;
+    
+        this.processes = [];
 
-  _init: function() {
-    this.createPanelMenu();
-  },
-  createPanelMenu: function () {
-    // creates icon button that will stay on the system status panel
-    this.ui = Ui.createStatusPanel({ iconPath: Me.path + '/assets/pm2-logo-light.svg' }, Lang.bind(this, function() {
-      this.updateProcessList();
-      return true;
-    }));
+        // creates icon button that will stay on the system status panel
+        this.ui = Ui.createStatusPanel(
+          this,
+          { iconPath: `${Me.path}/assets/pm2-logo-light.svg` }, 
+          Lang.bind(this, function() {
+            this.updateProcessList();
+            return true;
+          })
+        );
 
-    // prepare extension menu UI sections and default items
-    const { container: { menu }, section } = this.ui;
-    section.addMenuItem(Ui.createSimpleMenuItem('Loading...'));
-    menu.addMenuItem(Ui.createSeparatorMenuItem());
-      
-    const all = Ui.createSubMenu('All');
-    const allMenuEntries = [
-      { label: 'Start all', cb: this.comandForAll.bind(this, 'start') },
-      { label: 'Stop all', cb: this.comandForAll.bind(this, 'stop') },
-      { label: 'Resurrect', cb: this.comandForAll.bind(this, 'resurrect') },
-      { label: 'Save', cb: this.comandForAll.bind(this, 'save') },
-    ];
+        // prepare extension menu UI sections and default items
+        const { container: { menu }, section } = this.ui;
+        section.addMenuItem(Ui.createSimpleMenuItem('Loading...'));
+        menu.addMenuItem(Ui.createSeparatorMenuItem());
+        
+        const all = Ui.createSubMenu('All');
+        const allMenuEntries = [
+          { label: 'Start all', cb: this.comandForAll.bind(this, 'start') },
+          { label: 'Stop all', cb: this.comandForAll.bind(this, 'stop') },
+          { label: 'Resurrect', cb: this.comandForAll.bind(this, 'resurrect') },
+          { label: 'Save', cb: this.comandForAll.bind(this, 'save') },
+        ];
 
-    allMenuEntries.forEach(item => all.menu.addMenuItem(
-      Ui.createSimpleMenuItem(item.label, Lang.bind(this, item.cb))
-    ));
-
-    menu.addMenuItem(all);
-
-    // add extension button to the system status panel
-    Main.panel.addToStatusArea('pm2ProcessManager', this.ui.container);
-    Main.panel.connect('style-updated', Lang.bind(this, function(widget, user_data) { log('Received a style update from the system panel.'); }));
-  },
-  updateProcessList: async function () {
-    const { section } = this.ui;
-    try {
-      const processes = await (this.initCmdPromise ? this.initCmdPromise : PM2.getProcesses());
-      this.initCmdPromise = null;
-      section.removeAll();
-      processes.forEach(process => {
-        const { name, active } = PM2.getProcessInfo(process);
-        section.addMenuItem(Ui.createToggleMenuItem(
-          name, 
-          active, 
-          Lang.bind(this, this.toggleProcess.bind(this, process))
+        allMenuEntries.forEach(item => all.menu.addMenuItem(
+          Ui.createSimpleMenuItem(item.label, Lang.bind(this, item.cb))
         ));
-      });
 
-      if (!processes.length) {
-        const item = Ui.createSimpleMenuItem('No PM2 processes. If you previously saved, resurrect your PM2.');
-        section.addMenuItem(item);
+        menu.addMenuItem(all);
+      } catch (e) {
+        Lib.log(e);
       }
+    }
 
-    } catch (e) {
-      section.removeAll();
-      section.addMenuItem(Ui.createSimpleMenuItem('Error getting data from PM2. See gnome-shell logs.'));
-      Lib.log('Error reading PM2 processes:');
-      Lib.log(e);
-    }
-    return true;
-  },
-  toggleProcess: async function (process, menuItem, toggled) {
-    try {
-      if (toggled) {
-        await PM2.startProcess(process);
-      } else {
-        await PM2.stopProcess(process);
+    async updateProcessList() {
+      const { section } = this.ui;
+      try {
+        const processes = await (this.initCmdPromise ? this.initCmdPromise : PM2.getProcesses());
+        this.initCmdPromise = null;
+        section.removeAll();
+        processes.forEach(process => {
+          const { name, active } = PM2.getProcessInfo(process);
+          section.addMenuItem(Ui.createToggleMenuItem(
+            name, 
+            active, 
+            Lang.bind(this, this.toggleProcess.bind(this, process))
+          ));
+        });
+
+        if (!processes.length) {
+          const item = Ui.createSimpleMenuItem('No PM2 processes. If you previously saved, resurrect your PM2.');
+          section.addMenuItem(item);
+        }
+
+      } catch (e) {
+        section.removeAll();
+        section.addMenuItem(Ui.createSimpleMenuItem('Error getting data from PM2. See gnome-shell logs.'));
+        Lib.log('Error reading PM2 processes:');
+        Lib.log(e);
       }
-      menuItem.setToggleState(!toggled);
-    } catch (e) {
-      Lib.log(`Error ${toggled ? 'stopping' : 'starting'} PM2 processes:`);
-      Lib.log(e);
+      return true;
     }
-  },
-  comandForAll: async function (cmd) {
-    try {
-      const actions = {
-        start: PM2.startAll,
-        stop: PM2.stopAll,
-        resurrect: PM2.resurrect,
-        save: PM2.save,
-      };
-      await actions[cmd]();
-    } catch (e) {
-      Lib.log(`Error running ${cmd} for all PM2 processes:`);
-      Lib.log(e);
+  
+    async toggleProcess(process, menuItem, toggled) {
+      try {
+        if (toggled) {
+          await PM2.startProcess(process);
+        } else {
+          await PM2.stopProcess(process);
+        }
+        menuItem.setToggleState(!toggled);
+      } catch (e) {
+        Lib.log(`Error ${toggled ? 'stopping' : 'starting'} PM2 processes:`);
+        Lib.log(e);
+      }
     }
-  },
-  destroy: function () {
-    this.ui.container.destroy();
-  }
-});
+
+    async comandForAll(cmd) {
+      try {
+        const actions = {
+          start: PM2.startAll,
+          stop: PM2.stopAll,
+          resurrect: PM2.resurrect,
+          save: PM2.save,
+        };
+        await actions[cmd]();
+      } catch (e) {
+        Lib.log(`Error running ${cmd} for all PM2 processes:`);
+        Lib.log(e);
+      }
+    }
+
+    destroy() {
+      this.ui.container.destroy();
+    }
+  });
+
+
+var processManagerIndicator;
 
 /* exported enable */
-var processManager;
 function enable() {
-  processManager = new PM2ProcessManager();
+  processManagerIndicator = new PM2ProcessManager;
+  
+  // add extension button to the system status panel
+  Main.panel.addToStatusArea(STATUS_AREA_ROLE, processManagerIndicator);
 }
 
 /* exported disable */
 function disable() {
-  processManager.destroy();
+  processManagerIndicator.destroy();
 }
